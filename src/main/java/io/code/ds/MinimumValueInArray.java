2b23173aca2d58b944142b31148d753accdb9b6e
Shubham Kumar <shubham908@gmail.com>
2022-03-02 23:47:59 +0530

    Array 3 questions

---
+++ src/main/java/io/code/ds/MinimumValueInArray.java
@@ -0,0 +1,20 @@
+package io.code.ds;
+
+public class MinimumValueInArray {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {9,2,3,6};
+		int result = solve(arr);
+		System.out.println(result);
+	}
+	
+	public static int solve(int[] arr) {
+		int min = Integer.MAX_VALUE;
+		for (int i: arr) {
+			if (i <= min) {
+				min = i;
+			}
+		}
+		return min;
+	}
+}
